90cd03f5e6b4238206eb25dfa0d6d1ddfaf9bdc1
JensMonne <jensmonne@gmail.com>
2025-05-10 22:14:05 +0200

    i fricking forgot what i did, probably unimportant
    i did add the YAW unity package

---
+++ Assets/YawVR/Sample/ControlSamples/SimpleOrientationCopy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YawVR;
+/// <summary>
+/// Sets the YawTracker's orientation based on the GameObject's orientation
+/// </summary>
+public class SimpleOrientationCopy : MonoBehaviour
+{
+    /*
+       This script simply copies this gameObject's rotation, and sends it to the YawTracker
+    */
+    YawController yawController; // reference to YawController
+
+    private void Start() {
+        yawController = YawController.Instance();
+    }
+    private void FixedUpdate() {
+        yawController.TrackerObject.SetRotation(transform.localEulerAngles);
+    }
+}
---
+++ Assets/YawVR/Scripts/MotionCompensation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace YawVR {
+    /// <summary>
+    /// Cancels the camera's rotation based on IMU data
+    /// </summary>
+    public class MotionCompensation : MonoBehaviour
+    {
+        [SerializeField] private Transform cameraOffsetTransform;
+        [SerializeField] private YawController yawController;
+        [SerializeField] [Range(0f,0.9f)] private float smoothing = 0.7f;
+        private Vector3 simData;
+        private Vector3 offset;
+
+        public void UpdateOffset() {
+            offset.y = simData.y;
+        }
+
+        private void Update()
+        {
+            if (YawController.Instance().State != ControllerState.Started &&
+                YawController.Instance().State != ControllerState.Connected) return;
+            
+            simData.y = -yawController.Device.ActualPosition.yaw;
+
+            if (cameraOffsetTransform != null) {
+                cameraOffsetTransform.rotation = Quaternion.Slerp(cameraOffsetTransform.rotation,Quaternion.Euler(simData - offset),1f-smoothing);   
+            }
+        }
+    }
+}
