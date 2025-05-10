5d5eacd65948bcc4f54d769a18aeba9455a3ec1d
JensMonne <jensmonne@gmail.com>
2025-05-10 12:25:32 +0200

    smth

---
+++ Assets/Scripts/DebugSpeed.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class DebugSpeed : MonoBehaviour
+{
+    [SerializeField] private Rigidbody carRigidbody;
+    [SerializeField] private TMP_Text speedText;
+
+    private float currentSpeed = 0;
+
+    private void Update()
+    {
+        if (!Engine.isEngineRunning) return;
+        
+        currentSpeed = carRigidbody.linearVelocity.magnitude * 3.6f;
+        
+        speedText.text = $"Car speed = {currentSpeed:F2} km/h";
+    }
+}
