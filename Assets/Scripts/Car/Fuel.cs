b205549e5b20daf633801ea1e3d1d7b1d1a70385
daantje <daangulitz@gmail.com>
2025-05-23 13:45:56 +0200

    changes to level and mor power first gear

--- Assets/Scripts/Car/Fuel.cs
+++ Assets/Scripts/Car/Fuel.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Fuel : MonoBehaviour
 {
-     public static float maxfuelAmount = 100f;
+     public static float maxfuelAmount = 10000f;
      public static float currentFuelAmount;
      [SerializeField] private Rigidbody rb;
 
